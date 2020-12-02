8b8c6c031ed025430bb746a226a5fdc31f78d925
rutvej <waghela.rutvej1@gmail.com>
2020-12-02 14:35:07 +0530

    new modification

--- attendance.py
+++ attendance.py
@@ -1,15 +1,16 @@
 import pandas as pd
-
-attendance = pd.read_excel('Attendance sheet.xlsx')
-name=attendance['Name'].tolist()
-name = input('Enter ur Name:')
-if name in attendance['Name'].tolist():
-    date = attendance.columns
-    date = input('select the date'+',\n'.join(date)+'\n:')
-    if date in attendance.columns:
-        attendance = attendance.loc[attendance['Name']==name ,date]
-        print(attendance.values[0])
+while(True):
+    attendance = pd.read_excel('Attendance sheet.xlsx')
+    name=attendance['Name'].tolist()
+    name = input('Enter ur Name:')
+    if name in attendance['Name'].tolist():
+        date = attendance.columns
+        date = input('select the date'+',\n'.join(date)+'\n:')
+        if date in attendance.columns:
+            attendance = attendance.loc[attendance['Name']==name ,date]
+            print(attendance.values[0])
+        else:
+            print('attendance found!!')
     else:
-        print('attendance found!!')
-else:
-    print('Your not an employee')
+        print('Your not an employee')
+        continue if input('enter to continue or esc to exit')=='\n' else break
